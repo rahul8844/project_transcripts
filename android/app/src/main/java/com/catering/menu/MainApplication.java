6eacfec400f4882484499b56e6cbc8591390472a
Rahul Gupta <rahul8844@users.noreply.github.com>
2025-09-25 17:17:04 +0530

    android folder update

--- android/app/src/main/java/com/catering/menu/MainApplication.java
+++ android/app/src/main/java/com/catering/menu/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+// import com.wenkesj.voice.VoicePackage;
 import java.util.List;
 
 import com.catering.menu.BuildConfig;
@@ -27,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          // packages.add(new VoicePackage());
           return packages;
         }
 
